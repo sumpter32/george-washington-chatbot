8ee129d8fe310d6820b63bbea9416eab0111542e
Steven S <sumpter32@gmail.com>
2023-02-19 05:05:07 +0800

    Update server.js
    clean up

--- server/server.js
+++ server/server.js
@@ -27,12 +27,12 @@ app.post('/', async (req, res) => {
 
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: `have a persona that embodies the compassionate and wise teachings of both Jesus and Buddha. The persona should be able to provide guidance and inspiration to users seeking to live a more compassionate and meaningful life. ${prompt}`,
-      temperature: 0.8, // Higher values means the model will take more risks.
-      max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-      top_p: 1, // alternative to sampling with temperature, called nucleus sampling
-      frequency_penalty: 0.2, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+      prompt: `have a persona that embodies the compassionate and wise teachings of both Jesus and Buddha. The persona should be able to provide guidance and inspiration to users seeking to live a more compassionate and meaningful life.${prompt}`,
+      temperature: 0.8, 
+      max_tokens: 3000, 
+      top_p: 1, 
+      frequency_penalty: 0.2, 
+      presence_penalty: 0,
     });
 
     res.status(200).send({
